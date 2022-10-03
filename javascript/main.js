fbabff4369fd3c5b5f5a88ccdd3deef78371fd8a
dat123jqk <dattruong1209@gmail.com>
2022-10-03 15:51:08 +0700

    update code index and page furniture design, html,css,js

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".Modern-Slider").slick({
-      autoplay: true,
+      // autoplay: true,
       autoplaySpeed:3000,
       speed:700,
       mobileFirst: true,
@@ -19,5 +19,54 @@ $(document).ready(function() {
       autoplay: true,
       autoplaySpeed: 2000,
     });
+
+    $('.believe-content-feedback').slick({
+      infinite: false,
+      autoplay:true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
 })
 
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 500;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
